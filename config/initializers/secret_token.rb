beaaf6cc7b0331544441121dd5a0a517262b35a4
enter08 <demicredzep1990@gmail.com>
2014-02-20 15:43:03 +0100

    Successful deploy:install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog2::Application.config.secret_key_base = '5cf8ff6db8d3c3ff11949e7bdff8909bb521fb90447d7e807d51a9a2a164db4621eb981eeb9e1120dbaa252f990a0527e02a4abd814d79cc608543f3dca068c3'
---
+++ config/recipes/base.rb
@@ -0,0 +1,19 @@
+def template(from, to)
+	erb = File.read(File.expand_path("../templates/nginx_unicorn.erb"), __FILE__)
+	upload! ERB.new(erb).result(binding), to
+end
+
+def set_default(name, value)
+	if !fetch(name, false)
+		set(name, value)
+	end
+end
+
+namespace :deploy do
+	task :install do
+		on roles(:all) do
+			execute :sudo, "apt-get", "-y", "update"
+			execute :sudo, "apt-get", "-y", "install", "python-software-properties"
+		end
+	end
+end
